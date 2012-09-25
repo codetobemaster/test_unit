f18d74456e8e89cef24e3c795b950c323d7d4a64
tannv85 <tannv85@gmail.com>
2012-09-25 14:50:10 +0700

    fitsr initial rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestUnit::Application.config.secret_token = 'a37f5c1ee4352d860dad23aae5d3c99ebe0afb9533865ec985d6099d2fa8705026b76a68850eba1be910ff5d5886b4141bf46701c6fccfee6a683e543cb85671'
